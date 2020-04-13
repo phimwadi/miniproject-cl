22eb7ec02ca82aa8b590f5e8284a1c024b31d641
phimwadi <phimwadi0033@gmail.com>
2020-04-13 19:26:07 +0700

    commit login

---
+++ src/config.js
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: 'AIzaSyB6YY8r-vhGirwg4AmdCpQSWEXtnAOj8Co',
+    authDomain: 'miniproject-d6841.firebaseapp.com',
+    databaseURL: 'https://miniproject-d6841.firebaseio.com',
+    projectId: 'miniproject-d6841',
+    storageBucket: 'miniproject-d6841.appspot.com',
+    messagingSenderId: 'G-RPM15GN0HH'
+  }
+  
+  export default config
